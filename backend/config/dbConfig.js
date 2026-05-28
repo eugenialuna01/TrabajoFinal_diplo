2bfa3d73bff41880143efcbb010b813b0b201731
eugenia <eugeluna01@gmail.com>
2026-05-27 23:06:36 -0300

    Torneo

---
+++ backend/config/dbConfig.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+          dbName: "torneo",
+    });
+    console.log("Conexión exitosa a MongoDB");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB:", error);
+    process.exit(1);
+  } 
+};
+
